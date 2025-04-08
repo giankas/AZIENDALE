af2b43f8238b4cb5ef50cce46cda17d10d28b74d
giankas <lorenzogiani2@gmail.com>
2025-04-08 17:58:41 +0200

    INIZIO

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
- 
+package main
+
+import (
+	"github.com/giankas/AZIENDALE/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Registrazione di tutte le rotte
+	routes.RegisterRoutes(router)
+
+	// Avvio del server sulla porta 8080
+	router.Run(":8080")
+}
